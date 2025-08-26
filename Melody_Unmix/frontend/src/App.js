7fc1130b643b0a23a620413e674f556c1cad1a31
MarcoAnotino <marcojimm01@gmail.com>
2025-08-25 22:46:32 -0600

    chore(setup): initialize project environment and backend with Django, React, dependencies, and project structure

---
+++ Melody_Unmix/frontend/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/')
+    .then((res) => setData(res.data))
+    .catch((err) => console.error(err));
+  }, []);
+
+  if (!data) return <div>Loading...</div>;
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    
+    
+  );
+
+}
+
+export default App;
